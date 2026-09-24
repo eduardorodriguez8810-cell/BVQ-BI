cc7fbc044a5365f1766e851cbe66c917e461e798
eduardorodriguez8810-cell <eduardorodriguez8810@gmail.com>
2026-09-23 21:06:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Eduardo Rodríguez")
 
 archivo = st.file_uploader("Cargue su archivo")
+
 if archivo is not None:
-tabla = pd.read_csv(archivo)
-st.write(tabla)
+  tabla = pd.read_csv(archivo)
+  st.write(tabla)
