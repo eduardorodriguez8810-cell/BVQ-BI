4cbd33097398c8aaad63e708b37c5875e9f5901f
eduardorodriguez8810-cell <eduardorodriguez8810@gmail.com>
2026-09-23 21:17:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Eduardo Rodríguez")
 
-st.sidebar (archivo = st.file_uploader("Cargue su archivo"))
+archivo = st.file_uploader("Cargue su archivo")
 
 if archivo is not None:
   tabla = pd.read_csv(archivo)
