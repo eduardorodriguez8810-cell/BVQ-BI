5df1f72db12066ab23c14eaba4dbb4e4a12b72ea
eduardorodriguez8810-cell <eduardorodriguez8810@gmail.com>
2026-09-23 21:04:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Eduardo Rodríguez")
 
 archivo = st.file_uploader("Cargue su archivo")
+tabla = pd.read_csv(archivo)
+st.write(tabla)
